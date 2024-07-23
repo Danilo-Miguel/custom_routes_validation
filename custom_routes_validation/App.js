e24697dd027f0ee9c209806ef12d90a8eca3d026
unknown <danilo.miguel@cromai.com>
2024-07-22 22:40:09 -0300

    Feat: Change view to full scream

--- custom_routes_validation/App.js
+++ custom_routes_validation/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import Mapbox from '@rnmapbox/maps';
 
 Mapbox.setAccessToken('pk.eyJ1IjoiZGFuaWxvbWlndWVsMTQ4NSIsImEiOiJjbGZwYzg2ZzQwdW0yM3FwdG91Z3BoZXVtIn0.FOkbq1V7d5cjKTXgyTQVuQ');
 
 const App = () => {
+
+  const { width, height } = Dimensions.get('window');
+
   return (
-    <View style={styles.page}>
-      <View style={styles.container}>
+    <View style={[styles.page, {width, height}]}>
         <Mapbox.MapView style={styles.map} />
-      </View>
     </View>
   );
 }
@@ -19,13 +20,9 @@ export default App;
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+
   },
-  container: {
-    height: 300,
-    width: 300,
-  },
+ 
   map: {
     flex: 1
   }
